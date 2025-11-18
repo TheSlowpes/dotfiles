758da98b6d658df91461dae2a200a54d5cb67c11
Kevin Lopes <slowpes@gmail.com>
2025-11-18 07:36:09 -0500

    added additional configurations to oil.nvim to show lsp diagnostics and git signs

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -2,8 +2,14 @@ return {
   {
     "stevearc/oil.nvim",
     opts = {
+      win_options = {
+        signcolumn = "yes",
+      },
       keymaps = {
         ["<Esc><Esc>"] = { "actions.close", mode = "n" },
+        ["<S-j>"] = { "actions.select", opts = { horizontal = true } },
+        ["<S-l>"] = { "actions.select", opts = { vertical = true } },
+        ["<S-h>"] = { "actions.toggle_hidden" }
       },
       float = {
         max_width = 0.4,
@@ -16,6 +22,19 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
+  },
+  {
+    "JezerM/oil-lsp-diagnostics.nvim",
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+    opts = {},
+  },
+  {
+    "benomahony/oil-git.nvim",
+    dependencies = {
+      "stevearc/oil.nvim",
+    }
   },
   vim.keymap.set("n", "-", function()
     require("oil").open_float()
