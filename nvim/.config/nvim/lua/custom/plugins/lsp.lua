2a4ed576f905736084705fda7f49fa160d3f9642
Kevin Lopes <47455257+TheSlowpes@users.noreply.github.com>
2026-01-12 09:51:05 -0500

    Fix method call syntax for LSP client support checks
    client.supports_method is deprecated. Using client:supports_method instead

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -42,7 +42,7 @@ return {
           local client = vim.lsp.get_client_by_id(event.data.client_id)
 
           -- Document Highlight
-          if client and client.supports_method("textDocument/documentHighlight") then
+          if client and client:supports_method("textDocument/documentHighlight") then
             local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = true })
             vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
               buffer = event.buf,
@@ -57,7 +57,7 @@ return {
           end
 
           -- Inlay Hints
-          if client and client.supports_method("textDocument/inlayHint") then
+          if client and client:supports_method("textDocument/inlayHint") then
             map("<leader>th", function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
             end, "[T]oggle Inlay [H]ints")
